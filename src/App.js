437131e2e521465634636d411f518925b39b5eb3
bobbyhadz <opmftw@gmail.com>
2024-04-07 07:07:02 +0300

    minor changes

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+
+import {useState} from 'react';
+
+const App = () => {
+  const [message, setMessage] = useState('');
+
+  const handleChange = event => {
+    setMessage(event.target.value);
+
+    console.log('value is:', event.target.value);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        id="message"
+        name="message"
+        onChange={handleChange}
+        value={message}
+      />
+
+      <h2>Message: {message}</h2>
+    </div>
+  );
+};
+
+export default App;
